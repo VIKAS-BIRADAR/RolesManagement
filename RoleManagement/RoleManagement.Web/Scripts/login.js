6d1a68f9543ee2e15d749cb16edda019578c5520
VIKAS S BIRADAR <VikasS.Biradar@spektrasystems.com>
2021-05-31 19:15:30 +0530

    Password toggle

--- RoleManagement/RoleManagement.Web/Scripts/login.js
+++ RoleManagement/RoleManagement.Web/Scripts/login.js
@@ -1,14 +1,26 @@
 ﻿var App = angular.module("App", ['ngRoute']);
-App.config(['$routeProvider',function ($routeProvider) {
-    $routeProvider
-        .when('/login', {
-            templateUrl: 'Login.html',
-            controller:'loginController'
-        })
-      
-}]);
+
 
 App.controller("loginController", function ($scope, myService) {
+    $scope.inputType = "password",
+    $scope.showHideClass = "glyphicon glyphicon-eye-open"
+
+    $scope.showPassword = function () {
+        if ($scope.password != null) {
+            if ($scope.inputType == 'password') {
+                $scope.inputType = 'text',
+                $scope.showHideClass = 'glyphicon glyphicon-eye-close'
+            }
+            else
+            {
+                $scope.inputType = 'password',
+                    $scope.showHideClass = 'glyphicon glyphicon-eye-open'
+            }
+
+        }
+
+    }
+
     $scope.showError = false; 
     $scope.showSuccess = false;
 
@@ -22,7 +34,7 @@ App.controller("loginController", function ($scope, myService) {
         getData.then(function (msg) {
             if (msg.data == "1") {
                 flag = 1;
-                window.location.href = "/index.html";
+                window.location.href = "/dashboard.html";
 
             }
             else {
